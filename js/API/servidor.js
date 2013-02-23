bc2c296bb3212011c5f777b5434d806f4732b078
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-02-23 13:15:06 -0600

    logo y pantalla de fondo

---
+++ js/API/servidor.js
@@ -0,0 +1,17 @@
+// Conexion al servidor
+function logSend(nombre, lugar, mail, tel){
+$.ajax({
+	  type: "POST",
+	  url: "http://www.igitsoft.com/phtest.php",
+	  data: "nom="+nombre+"&lug="+lugar+"&mai="+mail+"&tel="+tel
+}).done(function( msg ) {
+  if(msg=="0")
+  	pgAlert('Error', 'Hubo un error al enviar datos');
+	else{
+		pgAlert('Registrado', 'Se ha registrado satisfactoriamente');
+		//Guardar Base de Datos local
+	}
+  
+ 
+});
+}
