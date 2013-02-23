f14f63c214024bb941b9231b62495365e1ab16e0
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-02-23 15:02:07 -0600

    0.0.9

--- js/API/notificaciones.js
+++ js/API/notificaciones.js
@@ -1,16 +1,4 @@
-// JavaScript Document
 //Notificaciones
 function pgAlert(tit,com){
 	navigator.notification.alert(com, null, tit, "Confirmado");
-	//otras notificaciones
-	/*
-	navigator.notification.confirm("Comentario", function(boton){
-		switch(boton){
-			case 1:
-			case 2:
-		}
-	},"titulo","Si,No,Talvez");
-	navigator,notification.beep(5);
-	*/
-	//navigator,notification.vibrate(100000);
 }
