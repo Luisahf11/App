6527e98c969c29c4e40fca2a57420ad80c5d492d
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-03-02 14:48:02 -0600

    0.0.23

--- js/API/captura.js
+++ js/API/captura.js
@@ -5,9 +5,9 @@ function capturaImg(){
 		//Imagen caturada
 		pgAlert('Captura de Imagen','Foto tomada Satisfactoriamente');
 		$('#regFoto').attr('rel',mediaFiles[0].fullPath).css('background-color','#0F0');
-		$('#login div[data-role=content] #regSubmit').prepend('<imgsrc="'+mediaFiles[0].fullPath+'" style="width:100%" />');
+		$('#login div[data-role=content] #regSubmit').prepend('<img src="'+mediaFiles[0].fullPath+'" style="width:100%" />');
 		},function(err){//error de captura
-	pgAlert('Error de captura',err.code);
+	pgAlert('Error de captura' + err.code);
 	}, {limit:1});
 	//Retorno
 	//return ruta;
