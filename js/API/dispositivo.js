87b74ead88fb8e70de91701e9480709b7bc8967f
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-03-02 11:37:43 -0600

    0.0.12
    captura de foto

---
+++ js/API/dispositivo.js
@@ -0,0 +1,18 @@
+//Dispositivo
+//var arr=new Arraay();
+//var arr=(1,2,3);
+function dispositivo(){
+	disp=[];
+	disp['name']=device.name;
+	disp['phonegap']=device.cordoba;
+	disp['plataform']=device.plataform;
+	disp['id']=device.uuid;
+	disp['version']=device.version;
+	disp['model']=device.model;
+	
+	return disp;
+}
+
+/*dispositivo()['name'];
+var dev=dispositivo();
+dev['name'];*/
--- js/API/servidor.js
+++ js/API/servidor.js
@@ -1,9 +1,9 @@
 // Conexion al servidor
-function logSend(nombre, lugar, mail, tel){
-$.ajax({
+function logSend(nombre,lugar,mail,tel){
+	$.ajax({
 	  type: "POST",
 	  url: "http://www.igitsoft.com/pgtest.php",
-	  data: "nom="+nombre+"&lug="+lugar+"&mai="+mail+"&tel="+tel
+	  data: "nom="+nombre+"&lug="+lugar+"&mai="+mail+"&tel="+tel+"&uuid="+dispositivo()['id']
 }).done(function( msg ) {
   if(msg=="0")
   	pgAlert('Error', 'Hubo un error al enviar datos');
