f216de320a44c3dfcf8fc213f1dde9e3149a4af3
Luis Alberto Hernandez <alberpapi_11062009@hotmail.com>
2013-03-09 15:13:58 -0600

    0.0.29

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -1,15 +1,15 @@
 // Conexion al servidor
 function logSend(nombre,lugar,mail,tel){
-	var disp=dospositivo();
+	var disp=dispositivo();
 	$.ajax({
 	  type: "POST",
 	  url: "http://www.igitsoft.com/pgtest.php",
 	  data: "nom="+nombre+"&lug="+lugar+"&mai="+mail+"&tel="+tel+"&uuid="+dispositivo()['id']
-}).done(function( msg ) {
+}).done(function(msg) {
   if(msg=="0")
   	pgAlert('Error', 'Hubo un error al enviar datos');
 	else{
-		subirArchivo($('#regFoto').attr('rel'));
+		subirArchivo($('#regFoto').attr('ruta'));
 		//pgAlert('Registrado', 'Se ha registrado satisfactoriamente');
 		//Guardar Base de Datos local
 	}
